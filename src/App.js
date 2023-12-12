9e8d42ff890a8e4d51bbe750ca3fe5ea49f4c0ed
Prajeesh <prajeeshrajan07@gmail.com>
2023-12-12 15:07:48 +0530

    Base leyer set completed

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+/**
+ * ?  =====Import Components=====
+ */
+import Home from './Pages/Home';
+
+function App() {
+  return (
+    <div>
+      <Home />
+    </div>
+  );
+  
+}
+
+export default App;
