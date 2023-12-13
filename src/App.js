8c98f3113c9fe4e1c6845e5db357dfa766d5393e
Prajeesh <prajeeshrajan07@gmail.com>
2023-12-13 15:06:20 +0530

    Initial Route Config Completed

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import './App.css';
-
-/**
- * ?  =====Import Components=====
- */
+import { BrowserRouter,Route,Routes} from 'react-router-dom'
 import Home from './Pages/Home';
+import Signup from './Pages/Signup';
+import Login from './Pages/Login'
 
 function App() {
   return (
     <div>
-      <Home />
+      <BrowserRouter>
+          <Routes>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/signup' element={<Signup/>}></Route>
+          <Route path='/login' element={<Login/>}></Route>
+          </Routes>
+      </BrowserRouter>
+
     </div>
-  );
-  
+
+  )
 }
 
 export default App;
